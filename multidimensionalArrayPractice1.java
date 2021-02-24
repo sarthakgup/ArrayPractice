bff977c761b663d5e7f205adbf90a3467d91a449
sarthakgup <16374350+sarthakgup@users.noreply.github.com>
2021-02-23 20:05:02 -0800

    first push with main method and basic functions

---
+++ multidimensionalArrayPractice1.java
@@ -0,0 +1,21 @@
+public class multidimensionalArrayPractice1
+{
+    public static void main(String args[])
+    {
+        System.out.println("hi");
+    }
+
+
+    public String[][] arrayGenerator(int rows, int cols)
+    {
+        String[][] myArr = new String[rows][cols];
+
+
+        return myArr;
+    }
+
+    //public String 3dFetcher(String[][] sourceArr, int row, int col)
+    //{
+    //    return sourceArr[row][col];
+    //}
+}
